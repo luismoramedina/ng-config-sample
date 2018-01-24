d1daf650d9ed8c53de096a9918c3fe582d3dbd95
luismoramedina <luismoramedina@gmail.com>
2018-01-24 17:44:07 +0100

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule
